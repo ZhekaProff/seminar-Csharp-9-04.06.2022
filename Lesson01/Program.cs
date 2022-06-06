87e207908eec512d25fe8e0964cedd98f3858227
ZhekaProff <Zhekakad@ya.ru>
2022-06-06 19:47:51 +0900

    решения

---
+++ Lesson01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Рекурсия. Показать натуральные числа от N до 1, N задано
+
+void PrintNumber(int n)
+{
+    Console.Write(n + " ");
+    n--;
+    if(n == 0) return;
+    PrintNumber(n);
+}
+
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0"); 
+PrintNumber(N);
+
+
+
---
+++ Lesson02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Рекурсия, Показать натуральные числа от 1 до N, N задано
+
+void PrintNumber(int n) // от 1 до N
+{
+    if (n < 1) return;
+    PrintNumber(n - 1);
+    Console.Write(n + " ");
+}
+
+void PrintNumbera(int n)  // от N до 1
+{
+    
+    if (n < 1) return;
+    Console.Write(n + " ");
+    PrintNumbera(n - 1);
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0"); 
+
+PrintNumber(N);
+Console.WriteLine();
+PrintNumbera(N);
